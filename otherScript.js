01961f0f5a53073f0b098596a2d13f0fc668d2d2
Somen Das <71995297+somen912@users.noreply.github.com>
2023-04-23 14:08:27 +0530

    Add files via upload

---
+++ otherScript.js
@@ -0,0 +1,87 @@
+let keyControl = document.querySelector(".task-bar input");
+let list = document.querySelector(".unorder-list");
+let saveButton = document.querySelector(".tool-bar i");
+let enterButton = document.querySelector(".task-bar button");
+
+const addNote = document.querySelector(".header-section button");
+
+
+
+keyControl.addEventListener("keyup",(event)=>{if(event.key=="Enter"){addToDo(keyControl.value); keyControl.value="";}});
+
+enterButton.addEventListener("click",()=>{addToDo(keyControl.value);keyControl.value="";})
+
+addNote.addEventListener("click",()=>{console.log("Add Note Button clicked");addnoteFunction();})
+
+
+
+saveButton.addEventListener("click",()=>{
+    let myArray = [];
+    let taskList = document.querySelectorAll(".unorder-list li");
+    taskList.forEach((element)=>{myArray.push(element.textContent)});
+    console.log(myArray);
+    localStorage.setItem("other-task",JSON.stringify(myArray));
+    alert("Task Saved");
+})
+
+
+
+
+const addToDo = (item) =>{const itemList = document.createElement("li");
+                          itemList.innerHTML=`${item}<i class="fa-solid fa-trash"></i>`;
+                          itemList.querySelector("i").addEventListener("click",()=>{itemList.remove()})
+                          list.appendChild(itemList);}
+
+
+// to recall saved data
+function getTask(){
+                          const pending = JSON.parse(localStorage.getItem("other-task"));
+                          console.log(pending);
+                          pending.forEach((element)=>{addToDo(element);})
+                                            }
+// calling data recall function
+getTask();
+
+// date time function
+const printDate = () =>{
+  let now = new Date()
+  document.querySelector(".date-time h2").innerText = now;
+}
+
+printDate();
+
+
+const addnoteFunction = (text = "") =>{
+  const note = document.createElement("div");
+  note.classList.add("note-pad");
+  note.innerHTML = `<div class="notepad-toolbar">
+    <i class="fa-solid fa-floppy-disk save"></i>
+    <i class="fa-solid fa-trash delete"></i>
+  </div>
+  <div class="text-area">
+   <textarea class="text-box">${text}</textarea>
+  </div> `;
+
+  document.querySelector(".notepad-section").appendChild(note);
+  note.querySelector(".notepad-toolbar .delete").addEventListener("click",()=>{console.log("Delete Clicked");note.remove();})
+  note.querySelector(".notepad-toolbar .save").addEventListener("click",()=>{console.log("Save Clicked");saveNotes(); alert("Notes saved");})
+}
+
+
+const saveNotes = () =>{
+  const notes = document.querySelectorAll(".note-pad textarea")
+  let myArray = [];
+  notes.forEach((note) => {myArray.push(`${note.value}`)});
+  console.log(myArray);
+  localStorage.setItem("other-notes", JSON.stringify(myArray));
+}
+
+
+const getlocalData = () => {
+  const lsnotes = JSON.parse(localStorage.getItem("other-notes"));
+  console.log(lsnotes);
+  lsnotes.forEach((notes) => { addnoteFunction(notes);
+  });
+}
+
+getlocalData();
